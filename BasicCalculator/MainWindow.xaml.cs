6a2465640433a0301e6188711029fb7291f2e975
AviramKing <134202816+AviramKing@users.noreply.github.com>
2024-05-31 18:57:13 +0300

    Add files via upload

---
+++ BasicCalculator/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sumBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int n1 = int.Parse(num1Txt.Text);
+            int n2 = int.Parse(num2Txt.Text);
+
+            int result = n1 + n2;
+            this.ResultTxt.Text = result.ToString();
+        }
+
+        private void subBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int n1 = int.Parse(num1Txt.Text);
+            int n2 = int.Parse(num2Txt.Text);
+
+            int result = n1 - n2;
+            this.ResultTxt.Text = result.ToString();
+        }
+
+        private void mulBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int n1 = int.Parse(num1Txt.Text);
+            int n2 = int.Parse(num2Txt.Text);
+
+            int result = n1 * n2;
+            this.ResultTxt.Text = result.ToString();
+        }
+
+        private void divBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int n1 = int.Parse(num1Txt.Text);
+            int n2 = int.Parse(num2Txt.Text);
+            if (n2 == 0)
+            {
+                MessageBox.Show("!אין לחלק ב- 0");
+            }
+            else
+            {
+                int result = n1 / n2;
+                this.ResultTxt.Text = result.ToString();
+            }
+        }
+
+
+    }
+}
